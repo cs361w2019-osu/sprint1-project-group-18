79e4791a11f802e3be89cfaccf64d10bec7aab98
brownit89 <35484067+brownit89@users.noreply.github.com>
2019-01-22 01:14:13 -0800

    Adding result class

---
+++ Result.java
@@ -0,0 +1,33 @@
+package cs361.battleships.models;
+
+public class Result {
+	private AtackStatus result;
+	private Ship ship;
+	private Square square;
+
+	public AtackStatus getResult() {
+		return result;
+	}
+
+	public void setResult(AtackStatus result) {
+		this.result = result;
+	}
+
+	public Ship getShip() {
+
+		return this.ship;
+	}
+
+	public void setShip(Ship ship) {
+		this.ship = ship;
+	}
+
+	public Square getLocation() {
+
+		return this.square;
+	}
+
+	public void setLocation(Square square) {
+		this.square = square;
+	}
+}
